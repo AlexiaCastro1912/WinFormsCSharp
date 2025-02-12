1cd40da8ebb69ffe612d7f1333a881461b4f3ff6
Alexia Castro <alexia.castro1912@outlook.com>
2025-02-12 11:52:01 -0600

    AreaPerimetro Circulo
    Lenguaje C#
    Visual Studio para WinForms

---
+++ 04 Area y Perimetro Circulo/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace _04_Area_y_Perimetro_Circulo
+{
+    partial class frmIndex
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmIndex));
+            this.btnCalcA = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnCalcP = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txtMedida = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCalcA
+            // 
+            this.btnCalcA.BackColor = System.Drawing.Color.Plum;
+            this.btnCalcA.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnCalcA.FlatAppearance.BorderSize = 3;
+            this.btnCalcA.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalcA.Location = new System.Drawing.Point(254, 55);
+            this.btnCalcA.Name = "btnCalcA";
+            this.btnCalcA.Size = new System.Drawing.Size(96, 55);
+            this.btnCalcA.TabIndex = 1;
+            this.btnCalcA.Text = "Calcular Area";
+            this.btnCalcA.UseVisualStyleBackColor = false;
+            this.btnCalcA.Click += new System.EventHandler(this.btnCalcA_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(364, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(140, 140);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnCalcP
+            // 
+            this.btnCalcP.BackColor = System.Drawing.Color.Plum;
+            this.btnCalcP.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnCalcP.FlatAppearance.BorderSize = 3;
+            this.btnCalcP.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalcP.Location = new System.Drawing.Point(254, 204);
+            this.btnCalcP.Name = "btnCalcP";
+            this.btnCalcP.Size = new System.Drawing.Size(96, 56);
+            this.btnCalcP.TabIndex = 1;
+            this.btnCalcP.Text = "Calcular Perimetro";
+            this.btnCalcP.UseVisualStyleBackColor = false;
+            this.btnCalcP.Click += new System.EventHandler(this.btnCalcP_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(364, 163);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(140, 140);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(9, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ingrese el radio del circulo";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Indigo;
+            this.panel3.Controls.Add(this.txtMedida);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Location = new System.Drawing.Point(12, 75);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(225, 175);
+            this.panel3.TabIndex = 2;
+            // 
+            // txtMedida
+            // 
+            this.txtMedida.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMedida.Location = new System.Drawing.Point(19, 79);
+            this.txtMedida.Name = "txtMedida";
+            this.txtMedida.Size = new System.Drawing.Size(179, 34);
+            this.txtMedida.TabIndex = 2;
+            // 
+            // frmIndex
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkViolet;
+            this.ClientSize = new System.Drawing.Size(520, 315);
+            this.Controls.Add(this.btnCalcP);
+            this.Controls.Add(this.btnCalcA);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "frmIndex";
+            this.Text = "04 AREA Y PERIMETRO - CIRCULO";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnCalcA;
+        private System.Windows.Forms.Button btnCalcP;
+        private System.Windows.Forms.TextBox txtMedida;
+    }
+}
+
---
+++ 04 Area y Perimetro Circulo/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _04_Area_y_Perimetro_Circulo
+{
+    public partial class frmIndex: Form
+    {
+        // INICIALIZAMOS VARIABLES
+        double vArea, vPerimetro;
+
+        public frmIndex()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcA_Click(object sender, EventArgs e)
+        {
+            // CALCULAR AREA DE LA FORMA BASICA
+            //vArea = 3.1416 * (Convert.ToDouble(txtMedida.Text) * Convert.ToDouble(txtMedida.Text));
+
+            //CALCULAR AREA USANDO CLASE MATH
+            // Libreria Math: Contiene Funciones para realizar calculos matematicos.
+            // Metodo Pow: Eleva un numero. Primer parametro es la variable u objeto, segundo parametro el numero elevado
+            // Metodo PI: Contiene el valor de PI (3.1416)
+            vArea = Math.PI * Math.Pow(Convert.ToDouble(txtMedida.Text), 2);
+            MessageBox.Show("El area del circulo es: " + vArea, "Area"); //MOSTRAMOS RESULTADO EN MESSAGEBOX
+        }
+
+        private void btnCalcP_Click(object sender, EventArgs e)
+        {
+            // OBTENER EL PERIMETRO DEL CIRCULO
+            vPerimetro = 2 * (Math.PI * Convert.ToDouble(txtMedida.Text));
+            MessageBox.Show("El perimetro del circulo es: " + vPerimetro, "Perimetro"); //MOSTRAMOS RESULTADO EN MESSAGEBOX
+        }
+    }
+}
