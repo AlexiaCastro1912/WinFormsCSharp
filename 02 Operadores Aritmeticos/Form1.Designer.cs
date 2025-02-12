3a9f5baf9083e6828781b3c2d8fd4e59dd7f0781
Alexia Castro <alexia.castro1912@outlook.com>
2025-02-12 00:18:00 -0600

    Operadores Aritmeticos
    Programa que realiza las 4 operaciones basicas.

---
+++ 02 Operadores Aritmeticos/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace _02_Operadores_Aritmeticos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnDivision = new System.Windows.Forms.Button();
+            this.btnMultiplicacion = new System.Windows.Forms.Button();
+            this.btnResta = new System.Windows.Forms.Button();
+            this.btnSuma = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.lblSigno = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.btnClose);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(562, 33);
+            this.panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label2.Location = new System.Drawing.Point(9, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(238, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "02 OPERADORES ARITMETICOS";
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.LightSalmon;
+            this.btnClose.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Location = new System.Drawing.Point(518, 0);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(44, 33);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.RoyalBlue;
+            this.panel2.Controls.Add(this.btnDivision);
+            this.panel2.Controls.Add(this.btnMultiplicacion);
+            this.panel2.Controls.Add(this.btnResta);
+            this.panel2.Controls.Add(this.btnSuma);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 33);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(148, 212);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnDivision
+            // 
+            this.btnDivision.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDivision.FlatAppearance.BorderSize = 0;
+            this.btnDivision.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDivision.ForeColor = System.Drawing.Color.White;
+            this.btnDivision.Location = new System.Drawing.Point(0, 144);
+            this.btnDivision.Name = "btnDivision";
+            this.btnDivision.Size = new System.Drawing.Size(148, 48);
+            this.btnDivision.TabIndex = 3;
+            this.btnDivision.Text = "DIVISION";
+            this.btnDivision.UseVisualStyleBackColor = true;
+            this.btnDivision.Click += new System.EventHandler(this.btnDivision_Click);
+            // 
+            // btnMultiplicacion
+            // 
+            this.btnMultiplicacion.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMultiplicacion.FlatAppearance.BorderSize = 0;
+            this.btnMultiplicacion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMultiplicacion.ForeColor = System.Drawing.Color.White;
+            this.btnMultiplicacion.Location = new System.Drawing.Point(0, 96);
+            this.btnMultiplicacion.Name = "btnMultiplicacion";
+            this.btnMultiplicacion.Size = new System.Drawing.Size(148, 48);
+            this.btnMultiplicacion.TabIndex = 2;
+            this.btnMultiplicacion.Text = "MULTIPLICACION";
+            this.btnMultiplicacion.UseVisualStyleBackColor = true;
+            this.btnMultiplicacion.Click += new System.EventHandler(this.btnMultiplicacion_Click);
+            // 
+            // btnResta
+            // 
+            this.btnResta.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnResta.FlatAppearance.BorderSize = 0;
+            this.btnResta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnResta.ForeColor = System.Drawing.Color.White;
+            this.btnResta.Location = new System.Drawing.Point(0, 48);
+            this.btnResta.Name = "btnResta";
+            this.btnResta.Size = new System.Drawing.Size(148, 48);
+            this.btnResta.TabIndex = 1;
+            this.btnResta.Text = "RESTA";
+            this.btnResta.UseVisualStyleBackColor = true;
+            this.btnResta.Click += new System.EventHandler(this.btnResta_Click);
+            // 
+            // btnSuma
+            // 
+            this.btnSuma.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSuma.FlatAppearance.BorderSize = 0;
+            this.btnSuma.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSuma.ForeColor = System.Drawing.Color.White;
+            this.btnSuma.Location = new System.Drawing.Point(0, 0);
+            this.btnSuma.Name = "btnSuma";
+            this.btnSuma.Size = new System.Drawing.Size(148, 48);
+            this.btnSuma.TabIndex = 0;
+            this.btnSuma.Text = "SUMA";
+            this.btnSuma.UseVisualStyleBackColor = true;
+            this.btnSuma.Click += new System.EventHandler(this.btnSuma_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(238, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(264, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "INGRESE LOS NUMEROS EN LAS CAJAS";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.txtNum1.Location = new System.Drawing.Point(173, 89);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(88, 53);
+            this.txtNum1.TabIndex = 3;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.txtNum2.Location = new System.Drawing.Point(312, 89);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(88, 53);
+            this.txtNum2.TabIndex = 4;
+            // 
+            // lblSigno
+            // 
+            this.lblSigno.AutoSize = true;
+            this.lblSigno.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSigno.ForeColor = System.Drawing.Color.White;
+            this.lblSigno.Location = new System.Drawing.Point(276, 100);
+            this.lblSigno.Name = "lblSigno";
+            this.lblSigno.Size = new System.Drawing.Size(21, 32);
+            this.lblSigno.TabIndex = 5;
+            this.lblSigno.Text = "|";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(418, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 32);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "=";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.BackColor = System.Drawing.Color.Black;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.ForeColor = System.Drawing.Color.LightSkyBlue;
+            this.lblResult.Location = new System.Drawing.Point(464, 92);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(86, 46);
+            this.lblResult.TabIndex = 7;
+            this.lblResult.Text = "000";
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnClear.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnClear.FlatAppearance.BorderSize = 0;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.ForeColor = System.Drawing.Color.White;
+            this.btnClear.Location = new System.Drawing.Point(148, 200);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(414, 45);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "LIMPIAR";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(562, 245);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblSigno);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnDivision;
+        private System.Windows.Forms.Button btnMultiplicacion;
+        private System.Windows.Forms.Button btnResta;
+        private System.Windows.Forms.Button btnSuma;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Label lblSigno;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ 02 Operadores Aritmeticos/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _02_Operadores_Aritmeticos
+{
+    public partial class Form1: Form
+    {
+        /// INICIALIZAMOS VARIABLES
+        int vResultado;
+
+        ///  CLASE DEL FORMULARIO
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void btnSuma_Click(object sender, EventArgs e)
+        {
+            /// CAMBIAMOS EL TEXTO DEL LABEL AL SIGNO ARITMETICO DE SUMA
+            lblSigno.Text = "+";
+
+            /// ESTA LINEA SOLO CONCATENA LOS TEXTOS QUE HAY EN CADA CAJA DE TEXTO
+            // lblResult.Text = txtNum1.Text + txtNum2.Text;
+
+            /// PARA CONVERTIR EL VALOR STRING A INTEGER DE LAS CAJAS DE TEXTO NECESITAMOS CREAR UNA VARIABLE DE TIPO ENTERO PARA ALMACENAR DICHO VALOR.
+            /// UTILIZAMOS EL METODO CONVERT PARA CONVERTIR EL VALOR INGRESADO EN LAS CAJAS DE TEXTO, QUE SON CADENAS, A NUMERO, ESTO EN CADA UNA DE LAS CAJAS.
+            /// SEGUIDO MOSTRAMOS EN UN LABEL EL RESULTADO ALMACENADO EN LA VARIABLE Y LO CONVERTIMOS A CADENA DE TEXTO CON TOSTRING.
+            vResultado = Convert.ToInt32(txtNum1.Text) + Convert.ToInt32(txtNum2.Text);
+            lblResult.Text = vResultado.ToString();
+        }
+
+        /// BOTON PARA REALIZAR UNA RESTA
+        private void btnResta_Click(object sender, EventArgs e)
+        {
+            lblSigno.Text = "-";
+
+            vResultado = Convert.ToInt32(txtNum1.Text) - Convert.ToInt32(txtNum2.Text);
+            lblResult.Text = vResultado.ToString();
+        }
+
+        /// BOTON PARA REALIZAR UNA MULTIPLICACION
+        private void btnMultiplicacion_Click(object sender, EventArgs e)
+        {
+            lblSigno.Text = "x";
+
+            vResultado = Convert.ToInt32(txtNum1.Text) * Convert.ToInt32(txtNum2.Text);
+            lblResult.Text = vResultado.ToString();
+        }
+
+        /// BOTON PARA REALIZAR UNA DIVISION
+        private void btnDivision_Click(object sender, EventArgs e)
+        {
+            lblSigno.Text = "/";
+
+            vResultado = Convert.ToInt32(txtNum1.Text) / Convert.ToInt32(txtNum2.Text);
+            lblResult.Text = vResultado.ToString();
+        }
+
+        /// BOTON PARA LIMPIAR TODOS LOS CAMPOS
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtNum1.Text = "";
+            txtNum2.Text = "";
+            lblResult.Text = "000";
+            lblSigno.Text = "|";
+        }
+
+        /// BOTON PARA CERRAR EL FORMULARIO. THIS HACE REFERENCIA A SI MISMO.
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
