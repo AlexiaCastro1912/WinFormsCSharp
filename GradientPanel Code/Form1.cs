4820ed905fdc07ff2193498776d966b63ba0922f
Alexia Castro <alexia.castro1912@outlook.com>
2025-03-06 08:08:38 -0600

    GradientPanel Code
    Codigo para implmentar degradado de colores a un control.

---
+++ GradientPanel Code/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GradientPanel_Code
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); // FINALIZAR LA EJECUCION DEL SISTEMA
+        }
+    }
+}
