0892c9980370f7ce7943a7573a9794613cc82cb7
Alexia Castro <alexia.castro1912@outlook.com>
2025-03-06 08:07:16 -0600

    Zodiac
    Practica del Signo Zodiacal

---
+++ Signo Zodiacal/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Signo_Zodiacal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbDiciembre = new System.Windows.Forms.RadioButton();
+            this.rbJulio = new System.Windows.Forms.RadioButton();
+            this.rbNoviembre = new System.Windows.Forms.RadioButton();
+            this.rbOctubre = new System.Windows.Forms.RadioButton();
+            this.rbSeptiembre = new System.Windows.Forms.RadioButton();
+            this.rbAgosto = new System.Windows.Forms.RadioButton();
+            this.rbJunio = new System.Windows.Forms.RadioButton();
+            this.rbMayo = new System.Windows.Forms.RadioButton();
+            this.rbAbril = new System.Windows.Forms.RadioButton();
+            this.rbMarzo = new System.Windows.Forms.RadioButton();
+            this.rbFebrero = new System.Windows.Forms.RadioButton();
+            this.rbEnero = new System.Windows.Forms.RadioButton();
+            this.cbDia = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pbZodiaco = new System.Windows.Forms.PictureBox();
+            this.btnObtener = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbZodiaco)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.rbDiciembre);
+            this.groupBox1.Controls.Add(this.rbJulio);
+            this.groupBox1.Controls.Add(this.rbNoviembre);
+            this.groupBox1.Controls.Add(this.rbOctubre);
+            this.groupBox1.Controls.Add(this.rbSeptiembre);
+            this.groupBox1.Controls.Add(this.rbAgosto);
+            this.groupBox1.Controls.Add(this.rbJunio);
+            this.groupBox1.Controls.Add(this.rbMayo);
+            this.groupBox1.Controls.Add(this.rbAbril);
+            this.groupBox1.Controls.Add(this.rbMarzo);
+            this.groupBox1.Controls.Add(this.rbFebrero);
+            this.groupBox1.Controls.Add(this.rbEnero);
+            this.groupBox1.Font = new System.Drawing.Font("Candara", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(290, 272);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "MES DE NACIMIENTO";
+            // 
+            // rbDiciembre
+            // 
+            this.rbDiciembre.AutoSize = true;
+            this.rbDiciembre.Location = new System.Drawing.Point(151, 233);
+            this.rbDiciembre.Name = "rbDiciembre";
+            this.rbDiciembre.Size = new System.Drawing.Size(114, 25);
+            this.rbDiciembre.TabIndex = 11;
+            this.rbDiciembre.Text = "DICIEMBRE";
+            this.rbDiciembre.UseVisualStyleBackColor = true;
+            // 
+            // rbJulio
+            // 
+            this.rbJulio.AutoSize = true;
+            this.rbJulio.Location = new System.Drawing.Point(151, 35);
+            this.rbJulio.Name = "rbJulio";
+            this.rbJulio.Size = new System.Drawing.Size(74, 25);
+            this.rbJulio.TabIndex = 10;
+            this.rbJulio.Text = "JULIO";
+            this.rbJulio.UseVisualStyleBackColor = true;
+            // 
+            // rbNoviembre
+            // 
+            this.rbNoviembre.AutoSize = true;
+            this.rbNoviembre.Location = new System.Drawing.Point(151, 193);
+            this.rbNoviembre.Name = "rbNoviembre";
+            this.rbNoviembre.Size = new System.Drawing.Size(122, 25);
+            this.rbNoviembre.TabIndex = 9;
+            this.rbNoviembre.Text = "NOVIMEBRE";
+            this.rbNoviembre.UseVisualStyleBackColor = true;
+            // 
+            // rbOctubre
+            // 
+            this.rbOctubre.AutoSize = true;
+            this.rbOctubre.Location = new System.Drawing.Point(151, 151);
+            this.rbOctubre.Name = "rbOctubre";
+            this.rbOctubre.Size = new System.Drawing.Size(101, 25);
+            this.rbOctubre.TabIndex = 8;
+            this.rbOctubre.Text = "OCTUBRE";
+            this.rbOctubre.UseVisualStyleBackColor = true;
+            // 
+            // rbSeptiembre
+            // 
+            this.rbSeptiembre.AutoSize = true;
+            this.rbSeptiembre.Location = new System.Drawing.Point(151, 112);
+            this.rbSeptiembre.Name = "rbSeptiembre";
+            this.rbSeptiembre.Size = new System.Drawing.Size(125, 25);
+            this.rbSeptiembre.TabIndex = 7;
+            this.rbSeptiembre.Text = "SEPTIEMBRE";
+            this.rbSeptiembre.UseVisualStyleBackColor = true;
+            // 
+            // rbAgosto
+            // 
+            this.rbAgosto.AutoSize = true;
+            this.rbAgosto.Location = new System.Drawing.Point(151, 74);
+            this.rbAgosto.Name = "rbAgosto";
+            this.rbAgosto.Size = new System.Drawing.Size(95, 25);
+            this.rbAgosto.TabIndex = 6;
+            this.rbAgosto.Text = "AGOSTO";
+            this.rbAgosto.UseVisualStyleBackColor = true;
+            // 
+            // rbJunio
+            // 
+            this.rbJunio.AutoSize = true;
+            this.rbJunio.Location = new System.Drawing.Point(7, 233);
+            this.rbJunio.Name = "rbJunio";
+            this.rbJunio.Size = new System.Drawing.Size(77, 25);
+            this.rbJunio.TabIndex = 5;
+            this.rbJunio.Text = "JUNIO";
+            this.rbJunio.UseVisualStyleBackColor = true;
+            // 
+            // rbMayo
+            // 
+            this.rbMayo.AutoSize = true;
+            this.rbMayo.Location = new System.Drawing.Point(6, 193);
+            this.rbMayo.Name = "rbMayo";
+            this.rbMayo.Size = new System.Drawing.Size(78, 25);
+            this.rbMayo.TabIndex = 4;
+            this.rbMayo.Text = "MAYO";
+            this.rbMayo.UseVisualStyleBackColor = true;
+            // 
+            // rbAbril
+            // 
+            this.rbAbril.AutoSize = true;
+            this.rbAbril.Location = new System.Drawing.Point(7, 151);
+            this.rbAbril.Name = "rbAbril";
+            this.rbAbril.Size = new System.Drawing.Size(75, 25);
+            this.rbAbril.TabIndex = 3;
+            this.rbAbril.Text = "ABRIL";
+            this.rbAbril.UseVisualStyleBackColor = true;
+            // 
+            // rbMarzo
+            // 
+            this.rbMarzo.AutoSize = true;
+            this.rbMarzo.Location = new System.Drawing.Point(7, 112);
+            this.rbMarzo.Name = "rbMarzo";
+            this.rbMarzo.Size = new System.Drawing.Size(88, 25);
+            this.rbMarzo.TabIndex = 2;
+            this.rbMarzo.Text = "MARZO";
+            this.rbMarzo.UseVisualStyleBackColor = true;
+            // 
+            // rbFebrero
+            // 
+            this.rbFebrero.AutoSize = true;
+            this.rbFebrero.Location = new System.Drawing.Point(7, 74);
+            this.rbFebrero.Name = "rbFebrero";
+            this.rbFebrero.Size = new System.Drawing.Size(99, 25);
+            this.rbFebrero.TabIndex = 1;
+            this.rbFebrero.Text = "FEBRERO";
+            this.rbFebrero.UseVisualStyleBackColor = true;
+            // 
+            // rbEnero
+            // 
+            this.rbEnero.AutoSize = true;
+            this.rbEnero.Location = new System.Drawing.Point(7, 35);
+            this.rbEnero.Name = "rbEnero";
+            this.rbEnero.Size = new System.Drawing.Size(82, 25);
+            this.rbEnero.TabIndex = 0;
+            this.rbEnero.Text = "ENERO";
+            this.rbEnero.UseVisualStyleBackColor = true;
+            // 
+            // cbDia
+            // 
+            this.cbDia.BackColor = System.Drawing.SystemColors.MenuText;
+            this.cbDia.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cbDia.Font = new System.Drawing.Font("Candara Light", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbDia.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.cbDia.FormattingEnabled = true;
+            this.cbDia.Location = new System.Drawing.Point(378, 40);
+            this.cbDia.Name = "cbDia";
+            this.cbDia.Size = new System.Drawing.Size(81, 36);
+            this.cbDia.TabIndex = 1;
+            this.cbDia.Text = "03";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Candara", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.label1.Location = new System.Drawing.Point(316, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "DIA DE NACIMIENTO";
+            // 
+            // pbZodiaco
+            // 
+            this.pbZodiaco.Image = ((System.Drawing.Image)(resources.GetObject("pbZodiaco.Image")));
+            this.pbZodiaco.Location = new System.Drawing.Point(540, 17);
+            this.pbZodiaco.Name = "pbZodiaco";
+            this.pbZodiaco.Size = new System.Drawing.Size(270, 270);
+            this.pbZodiaco.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbZodiaco.TabIndex = 3;
+            this.pbZodiaco.TabStop = false;
+            // 
+            // btnObtener
+            // 
+            this.btnObtener.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnObtener.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnObtener.Font = new System.Drawing.Font("Candara", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnObtener.ForeColor = System.Drawing.Color.White;
+            this.btnObtener.Location = new System.Drawing.Point(342, 218);
+            this.btnObtener.Name = "btnObtener";
+            this.btnObtener.Size = new System.Drawing.Size(155, 65);
+            this.btnObtener.TabIndex = 4;
+            this.btnObtener.Text = "OBTENER SIGNO ZODIACAL";
+            this.btnObtener.UseVisualStyleBackColor = false;
+            this.btnObtener.Click += new System.EventHandler(this.btnObtener_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(828, 301);
+            this.Controls.Add(this.btnObtener);
+            this.Controls.Add(this.pbZodiaco);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbDia);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "SIGNO ZODIACAL";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbZodiaco)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbDiciembre;
+        private System.Windows.Forms.RadioButton rbJulio;
+        private System.Windows.Forms.RadioButton rbNoviembre;
+        private System.Windows.Forms.RadioButton rbOctubre;
+        private System.Windows.Forms.RadioButton rbSeptiembre;
+        private System.Windows.Forms.RadioButton rbAgosto;
+        private System.Windows.Forms.RadioButton rbJunio;
+        private System.Windows.Forms.RadioButton rbMayo;
+        private System.Windows.Forms.RadioButton rbAbril;
+        private System.Windows.Forms.RadioButton rbMarzo;
+        private System.Windows.Forms.RadioButton rbFebrero;
+        private System.Windows.Forms.RadioButton rbEnero;
+        private System.Windows.Forms.ComboBox cbDia;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pbZodiaco;
+        private System.Windows.Forms.Button btnObtener;
+    }
+}
+
